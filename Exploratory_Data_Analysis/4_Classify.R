eef59839e23c1953b534290aee089b3182f0087c
Ross Petchler <ross.petchler@gmail.com>
2012-06-20 06:38:10 -0400

    Finished Naive Bayes classifier. Added resampling tests.

--- Exploratory_Data_Analysis/4_Classify.R
+++ Exploratory_Data_Analysis/4_Classify.R
@@ -2,20 +2,11 @@
 #
 #
 
+
 options(stringsAsFactors = FALSE)
 
+library(ggplot2)
 library(tm)
-
-
-judgements <- readLines("../Data/Training_Data_Judgements.txt")
-
-tweets <- readLines("../Data/Training_Data_Processed.txt", encoding = "UTF-8")
-
-
-control <- list(stopwords = TRUE,
-                removePunctuation = TRUE,
-                removeNumbers = TRUE,
-                minDocFreq = 2)
 
 
 ComputeTermFrequencies <- function(tdm) {
@@ -24,44 +15,64 @@ ComputeTermFrequencies <- function(tdm) {
   df$occurs <- sapply(seq(nrow(mat)),
                       function(x) length(which(mat[x, ] > 0)) / ncol(mat))
   df$density <- df$frequency / sum(df$frequency)
-  return(df[order(-df$occurs), ])
+  return(df)
 }
 
-
-corpus <- Corpus(VectorSource(tweets[judgements == "y"]))
-tdm <- TermDocumentMatrix(corpus, control)
-y.term.frequencies <- ComputeTermFrequencies(tdm)
-
-corpus <- Corpus(VectorSource(tweets[judgements == "n"]))
-tdm <- TermDocumentMatrix(corpus, control)
-n.term.frequencies <- ComputeTermFrequencies(tdm)
-
-
-ClassifyTweet <- function(tweet, training.df, prior = .5, c = 1e-6) {
+ClassifyTweet <- function(tweet, training.df, prior = .5, c = 1e-4) {
   corpus <- Corpus(VectorSource(tweet))
   tdm <- TermDocumentMatrix(corpus, control)
   freq <- rowSums(as.matrix(tdm))
   intersection <- intersect(rownames(freq), training.df$term)
-  if (length(intersection) == 0) {
+  if (length(intersection) < 1) {
     return(prior * c ^ length(freq))
   } else {
-    probs <- training.df$occurance[match(intersection, training.df$term)]
-    return(prior * prod(match.probs * c ^ (length(freq) - length(intersection))))
+    probs <- training.df$occurs[match(intersection, training.df$term)]
+    return(prior * prod(probs * c ^ (length(freq) - length(intersection))))
   }
 }
 
 
-s <- sample.int(length(judgements), 50)
-testing.data <- data.frame(tweets = tweets[s], judgements = judgements[s])
+judgements <- readLines("../Data/Training_Data_Judgements.txt")
+tweets <- readLines("../Data/Training_Data_Processed.txt", encoding = "UTF-8")
 
-y.test <- sapply(testing.data$tweets, ClassifyTweet,
-                 training.df = y.term.frequencies)
-n.test <- sapply(testing.data$tweets, ClassifyTweet,
-                 training.df = n.term.frequencies)
+# Drop ambiguous judgements.
+tweets <- tweets[judgements %in% c("y", "n")]
+judgements <- judgements[judgements %in% c("y", "n")]
 
-res <- y.test > n.test
-table(res)
+# Use the size of the smallest set of judgements as the size of the training
+# data sets. This imposes the assumption of equal prior probabilities.
+training.length <- min(sum(judgements == "y"), sum(judgements == "n"))
 
-table(testing.data$judgements)
+control <- list(tolower = TRUE,
+                removePunctuation = TRUE,
+                removeNumbers = TRUE,
+                stopwords = TRUE,
+                stemming = TRUE,
+                minDocFreq = 2)
+
+# Compute training data sets for "yes" and "no" judgements.
+y.training.tweets <- sample(tweets[judgements == "y"], training.length)
+corpus <- Corpus(VectorSource(y.training.tweets))
+tdm <- TermDocumentMatrix(corpus, control)
+y.training.df <- ComputeTermFrequencies(tdm)
+
+n.training.tweets <- sample(tweets[judgements == "n"], training.length)
+corpus <- Corpus(VectorSource(n.training.tweets))
+tdm <- TermDocumentMatrix(corpus, control)
+n.training.df <- ComputeTermFrequencies(tdm)
+
+# Test classifier on a sample of judgements. Use priors learned from
+# repeated trials.
+for (trial in 1:10) {
+  s <- sample.int(length(judgements), 75)
+  testing.df <- data.frame(tweet = tweets[s], judgement = judgements[s])
+  testing.df$prob.y <- sapply(testing.df$tweet, ClassifyTweet,
+                              training.df = y.training.df, prior = .15)
+  testing.df$prob.n <- sapply(testing.df$tweet, ClassifyTweet,
+                              training.df = n.training.df, prior = .85)
+  testing.df <- within(testing.df, guess <- ifelse(prob.y > prob.n, "y", "n"))
+  cat("\n\nTrial ", trial, ":\n", sep = "")
+  print(with(testing.df, table(judgement, guess)))
+}
 
 
